b600876fa33fc76752e41a5f384a0164c3fc9608
Kris Pennimpede <kpennimpede@hotmail.com>
2021-02-13 12:19:35 -0500

    Can create and get each attribute of the employee object

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,11 +1,33 @@
 const Employee = require('../lib/Employee');
 
 test('create an Employee Object', () => {
-  const employee = new Employee('Jim');
-  employee.id = 123;
-  employee.email = 'test@test.com'
-
+  const employee = new Employee('Jim', 123, 'test');
+  
   expect(employee.name).toBe('Jim');
   expect(employee.id).toEqual(expect.any(Number));
   expect(employee.email).toEqual(expect.any(String));
+});
+
+test('get the employee name', () => {
+  const employee = new Employee('Jim', 123, 'test');
+
+  expect(employee.getName()).toBe('Jim');
+});
+
+test('get the employee id', () => {
+  const employee = new Employee('Jim', 123, 'test');
+
+  expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+test('get the employee email', () => {
+  const employee = new Employee('Jim', 123, 'test');
+
+  expect(employee.getEmail()).toEqual(expect.any(String));
+});
+
+test('get the employees role', () => {
+  const employee = new Employee('Jim', 123, 'test');
+
+  expect(employee.getRole()).toBe('Employee');
 })
