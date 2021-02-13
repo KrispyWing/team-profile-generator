40cb6356218d21c3d593677337319b3e62e0957f
Kris Pennimpede <kpennimpede@hotmail.com>
2021-02-13 18:43:50 -0500

    ask for team info and save data to an array

--- index.js
+++ index.js
@@ -10,22 +10,22 @@ function addManager() {
   inquirer
     .prompt([
     {
-      type: 'text',
+      type: 'input',
       name: 'name',
       message: 'What is your name?'
     },
     {
-      type: 'text',
+      type: 'input',
       name: 'id',
       message: 'Enter your employee ID number:'
     },
     {
-      type: 'text',
+      type: 'input',
       name: 'email',
       message: 'Enter your email address:'
     },
     {
-      type: 'text',
+      type: 'input',
       name: 'officeNumber',
       message: 'Enter your office number:'
     }
@@ -35,9 +35,118 @@ function addManager() {
     console.log(manager);
     team.push(manager);
     console.log(team);
-  })  
+    addTeamMember();
+  })
   
 };
 
-addManager();
+function addTeamMember() {
+  inquirer
+    .prompt([
+    {
+      type: 'list',
+      name: 'role',
+      message: 'What Role is the employee?',
+      choices: ['Engineer', 'Intern']
+    }
+    ])
+    .then(function(data) {
+      switch (data.role) {
+        case "Engineer":
+          addEngineer();
+          break;
+        
+        case "Intern":
+          addIntern();
+          break;
+      }
+    });
+  };
+
+function addEngineer() {
+  inquirer
+    .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Enter your Engineers name?'
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'Enter your Engineers ID number:'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter the Engineers email address:'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter the Engineers github username:'
+    }
+  ])
+  .then(function({name, id, email, github}) {
+    const engineer = new Engineer(name, id, email, github);
+    console.log(engineer);
+    team.push(engineer);
+    console.log(team);
+    addMore();
+  })
+};
+
+function addIntern() {
+  inquirer
+    .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Enter your Interns name?'
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'Enter your Interns ID number:'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter the Interns email address:'
+    },
+    {
+      type: 'input',
+      name: 'school',
+      message: 'Enter the Interns School:'
+    }
+  ])
+  .then(function({name, id, email, school}) {
+    const intern = new Intern(name, id, email, school);
+    console.log(intern);
+    team.push(intern);
+    console.log(team);
+    addMore();
+  })
+};
+
+function addMore() {
+  inquirer.prompt([
+    {
+      type: 'confirm',
+      name: 'addmore',
+      message: 'Would you like to add another team member?'
+    }
+  ])
+  .then(function({ addmore }) {
+    if (addmore) {
+      addTeamMember();
+    } else {
+      return; // create page function will get called here
+    }
+  })
+}
+    
+
+addManager()
+
 
